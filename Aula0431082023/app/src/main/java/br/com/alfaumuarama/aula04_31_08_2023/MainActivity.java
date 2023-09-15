5e7060306c461760adcad02d0433e9dce2f3beb9
Monicakazumi <106854565+Monicakazumi@users.noreply.github.com>
2023-09-14 22:15:34 -0300

    Add files via upload

--- Aula0431082023/app/src/main/java/br/com/alfaumuarama/aula04_31_08_2023/MainActivity.java
+++ Aula0431082023/app/src/main/java/br/com/alfaumuarama/aula04_31_08_2023/MainActivity.java
@@ -2,16 +2,23 @@ package br.com.alfaumuarama.aula04_31_08_2023;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ListActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.concurrent.ExecutionException;
 
 import br.com.alfaumuarama.aula04_31_08_2023.datasource.BuscarDadosApi;
 import br.com.alfaumuarama.aula04_31_08_2023.models.Pokemon;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends ListActivity {
 
     private ArrayList<Pokemon> listaDados;
 
@@ -23,11 +30,44 @@ public class MainActivity extends AppCompatActivity {
         //cria a thread, executa e pega o retorno
         try {
             listaDados = new BuscarDadosApi().execute(Config.link).get();
+
+            ListAdapter adapter = new SimpleAdapter(this, //this - onde a clase que tem o objeto listView no XML
+                    dadosToMap(listaDados), //lista com os dados em formato HashMap
+                    R.layout.listview_modelo, //layout de modelo para cada item da lista
+                    new String[] { "nome" },  //campo dos dados que sera carregado na listaDados
+                    new int[] { R.id.txtNome } //em que item da lista carregara os dados do listview
+            );
+
+            setListAdapter(adapter);
+
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
 
-        Pokemon pokemon = listaDados.get(0);
-        Toast.makeText(this, pokemon.nome, Toast.LENGTH_SHORT).show();
+        //Pokemon pokemon = listaDados.get(0);
+        //Toast.makeText(this, pokemon.nome, Toast.LENGTH_SHORT).show();
+    }
+
+    private List<HashMap<String,String>> dadosToMap(ArrayList<Pokemon> listaDados) {
+        List<HashMap<String,String>> lista = new ArrayList<>();
+
+        for (int i = 0; i< listaDados.size(); i++) {
+            HashMap<String,String> item = new HashMap<>();
+            item.put("id", String.valueOf(listaDados.get(i).id()));
+            item.put("nome", listaDados.get(i).nome);
+            item.put("imagem", listaDados.get(i).imagem());
+
+            lista.add(item);
+        }
+
+        return lista;
+    }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+        
+        //positon -> tem o indice do item que foi clicado na ListView
+        Pokemon pokemon = listaDados.get(position);
     }
 }
