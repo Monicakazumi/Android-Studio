57c9198d042d2d07a5915328a5079e725e687236
Monicakazumi <106854565+Monicakazumi@users.noreply.github.com>
2023-10-05 22:13:21 -0300

    Add files via upload

---
+++ aula0705102023/app/src/main/java/br/com/alfaumuarama/aula07_05_10_2023/models/Aluno.java
@@ -0,0 +1,38 @@
+package br.com.alfaumuarama.aula07_05_10_2023.models;
+
+import android.content.ContentValues;
+
+import java.util.HashMap;
+
+public class Aluno {
+    public int id;
+    public String nome;
+    public String ra;
+
+
+    //mapa de dados - método
+    //Converte os dados de classe em ContentValues (padrao SQLite)
+    public ContentValues toDados() {
+        ContentValues dados = new ContentValues();
+
+        if (id > 0) {
+            dados.put("id", id);
+        }
+        dados.put("nome", nome);
+        dados.put("ra", ra);
+
+        return dados;
+    }
+
+    //Converter os dados da classe em HashMap<> (padrao do ListView)
+    public HashMap<String,String> toMap() {
+        HashMap<String,String> dados = new HashMap<>();
+
+
+        dados.put("id", String.valueOf(id));
+        dados.put("nome", nome);
+        dados.put("ra", ra);
+
+        return dados;
+    }
+}
